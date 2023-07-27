dd9eec9018c287554cae62aed88a10417f2eaa3b
Jochen Martin Eppler <jougs@gmx.net>
2023-07-27 19:19:03 +0200

    Make code more generic and add a default blast gate

--- apps/dust_collection.py
+++ apps/dust_collection.py
@@ -4,6 +4,7 @@ class DustCollection(hass.Hass):
 
     blast_gates = ["Maslow", "Work bench", "Table saw", "Mitre saw"]
     topic = "blast_gate/"
+    input_select = "input_select.blast_gate"
 
     def initialize(self):
 
@@ -15,8 +16,7 @@ class DustCollection(hass.Hass):
         self.fire_event('service_registered', domain="input_select", service="select_previous")
         self.fire_event('service_registered', domain="input_select", service="select_next")
 
-        entity = "input_select.blast_gate"
-        self.set_state(entity, state='x', attributes={
+        self.set_state(self.input_select, state='Table saw ', attributes={
             'options': self.blast_gates,
             'friendly_name': 'Blast gate',
             'icon': 'mdi:valve'
@@ -25,11 +25,14 @@ class DustCollection(hass.Hass):
             'event': 'call_service',
             'domain': 'input_select',
             'service': 'select_option',
-            'entity': entity
+            'entity': self.input_select
         })
 
 
     def select_input(self, event_name, data, kwargs):
+
+        if data["service_data"]["entity_id"] != self.input_select:
+            return
 
         msg = f'event_name={event_name}, data={data}, kwargs={kwargs}'
         self.log('select_input()', msg)
@@ -40,3 +43,5 @@ class DustCollection(hass.Hass):
             activation = 0.6 if gate == active_gate else -0.75
             self.log('select_input()', f"sending {activation} on topic {topic}")
             self.call_service("mqtt/publish", topic=topic, payload=str(activation))
+
+        self.set_state(self.input_select, state=active_gate)
