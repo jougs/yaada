ea69357e7170a0b63fabf8702b3a7cefcf92e1c1
Jochen Martin Eppler <jougs@gmx.net>
2023-08-28 01:27:30 +0200

    Actually ring and make the effect a bit more snappy

--- apps/doorbell.py
+++ apps/doorbell.py
@@ -4,39 +4,36 @@ from time import sleep
 
 class Doorbell(hass.Hass):
 
-    lights = [
-        "light.downstairs_hallway_stele_1",
-        "light.downstairs_hallway_stele_2",
-        "light.downstairs_hallway_stele_3",
-        "light.downstairs_hallway_stele_4",
-        "light.downstairs_hallway_stele_5",
-        "light.downstairs_hallway_stele_6",
-        "light.downstairs_hallway_stele_7",
-    ]
-
-    trans_time = 0.5
+    lights = [f"light.downstairs_hallway_stele_{i}" for i in range(1,8)]
+    trans_time = 0.25
 
     def initialize(self):
+
 
         self_log = self.log
         self.log = lambda func, msg: self_log(f'{func}: {msg}')
 
-        self.listen_state(self.trigger, "input_boolean.doorbell_effect", attribute='all')
+        self.listen_event(self.trigger, 'MQTT_MESSAGE', topic='input', namespace='mqtt')
 
 
-    def trigger(self, trigger, attribute, old, new, kwargs):
-        
-        self.log("trigger()", f"trigger={trigger}, new={new}")
+    def trigger(self, event_name, data, kwargs):
 
-        if new["state"] == "off":
+        if data['payload'] != "doorbell_short":
             return
-        
-        for light in reversed(self.lights):
+
+        self.turn_on("switch.doorbell") # Turns itself off again after 1.5s
+
+        self.sequence(reversed(self.lights), (255, 0, 0, 0))
+
+
+    def sequence(self, lights, color):
+
+        for light in lights:
             old_state = self.get_state(light, attribute="all")
 
             attributes = {
                 'transition': self.trans_time,
-                "rgbw_color": (255, 0, 0, 0),
+                "rgbw_color": color,
                 "brightness": 255,
             }
             self.turn_on(light, **attributes)
@@ -47,13 +44,10 @@ class Doorbell(hass.Hass):
                     attributes[key] = old_state["attributes"][key]
                 self.run_in(self.turn_light_on, self.trans_time, light=light, attributes=attributes)
             else:
-                self.run_in(self.turn_light_off, self.trans_time, light=light)
-
-        self.set_state("input_boolean.doorbell_effect", state="off")
-
+               self.run_in(self.turn_light_off, self.trans_time, light=light)
 
     def turn_light_on(self, kwargs):
-        
+
         self.turn_on(kwargs["light"], **kwargs["attributes"])
 
     def turn_light_off(self, kwargs):
