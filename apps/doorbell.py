45d7d0cf93dbd09929ff486bb5423f91661e848a
Jochen Martin Eppler <jougs@gmx.net>
2023-09-07 09:41:29 +0200

    Respect mode selection and add telegram notification for the doorbell

--- apps/doorbell.py
+++ apps/doorbell.py
@@ -9,27 +9,36 @@ class Doorbell(hass.Hass):
 
     def initialize(self):
 
-
         self_log = self.log
         self.log = lambda func, msg: self_log(f'{func}: {msg}')
 
         self.listen_event(self.trigger, 'MQTT_MESSAGE', topic='input', namespace='mqtt')
-
 
     def trigger(self, event_name, data, kwargs):
 
         if data['payload'] != "doorbell_short":
             return
 
-        self.turn_on("switch.doorbell") # Turns itself off again after 1.5s
+        doorbell_state = self.get_state("input_select.doorbell_mode", attribute="state")
 
-        self.sequence(reversed(self.lights), (255, 0, 0, 0))
+        if "Ring" in doorbell_state:
+            self.turn_on("switch.doorbell") # Turns itself off again after 1.5s, see ESPHome config
+
+        if "Visual" in doorbell_state:
+            self.sequence(reversed(self.lights), (255, 0, 0, 0))
+
+        msg_data = {
+            "target": self.args["telegram_target"],
+            "message": f"The doorbell rang!",
+        }
+        self.call_service("telegram_bot/send_message", **msg_data)
 
 
     def sequence(self, lights, color):
 
-        for light in lights:
+        for i, light in enumerate(lights):
             old_state = self.get_state(light, attribute="all")
+            old_attrs = old_state["attributes"]
 
             attributes = {
                 'transition': self.trans_time,
@@ -39,12 +48,10 @@ class Doorbell(hass.Hass):
             self.turn_on(light, **attributes)
 
             if old_state['state'] == "on":
-                attributes = {"transition": self.trans_time}
-                for key in ("brightness", "rgbw_color", "brightness"):
-                    attributes[key] = old_state["attributes"][key]
+                attributes = {"transition": self.trans_time} | {key: old_attrs[key] for key in ("brightness", "rgbw_color")}
                 self.run_in(self.turn_light_on, self.trans_time, light=light, attributes=attributes)
             else:
-               self.run_in(self.turn_light_off, self.trans_time, light=light)
+                self.run_in(self.turn_light_off, self.trans_time, light=light)
 
     def turn_light_on(self, kwargs):
 
